396bd1f4ec1615fa9ddeee4fce3b9208a368897a
liweichao <weichao092@163.com>
2018-04-15 23:56:40 +0800

    add check svc

---
+++ bin/check-svc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+which svc
+if [[ $? != 0 ]]; then
+   	apt-get update
+   	apt install daemontools
+	if [ ! -f /lib/systemd/system/svscan.service ]; then
+	    echo $'[Unit]\nDescription=Job that for svscan\n\n[Service]\nType=forking\nExecStart=/usr/bin/svscanboot\nRestart=on-failure\n\n[Install]\nWantedBy=multi-user.target' > /lib/systemd/system/svscan.service
+		systemctl daemon-reload
+		systemctl start svscan
+	fi
+fi
